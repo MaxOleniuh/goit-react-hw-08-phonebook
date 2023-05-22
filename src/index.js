5b06d587c781937692051867af095c76c908dbf1
oleniuh <maxim.oleniukh@gmail.com>
2023-05-22 17:39:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         theme={{ colorScheme: 'dark' }}
       >
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <BrowserRouter basename="goit-react-hw-08-phonebook">
             <App />
           </BrowserRouter>
         </PersistGate>
