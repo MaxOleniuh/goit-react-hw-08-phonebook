abbdd82d8cef343bc3f26289d00993218a8cb4bf
oleniuh <maxim.oleniukh@gmail.com>
2023-05-23 00:12:06 +0100

    aos animations

--- src/pages/MainLayout.jsx/MainLayout.jsx
+++ src/pages/MainLayout.jsx/MainLayout.jsx
@@ -3,36 +3,44 @@ import Form from '../../components/Form/Form';
 import List from '../../components/List/List';
 import Filter from '../../components/Filter/Filter';
 import { SimpleGrid, Text, Title } from '@mantine/core';
-import PhonebookWrapper from './MainLayout.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContacts, getIsLoading } from 'redux/contacts/selectors';
 import { fetchContacts } from 'redux/contacts/operations';
 import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const MainLayout = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
+
   const items = useSelector(getContacts);
   const { isLoading } = useSelector(getIsLoading);
     const filter = useSelector(state => state.filter);
   const filteredContacts = () => items.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
   
+
+  useEffect(() => {
+    AOS.init({
+      duration: 500,
+    });
+  }, []);
   return (
     <>
         {isLoading && <Loader />}
-        <PhonebookWrapper>
         <SimpleGrid cols={1} spacing="md">
-          <Title fw={900} fz={62}>Phonebook</Title>
+          <Title fw={900} fz={48} data-aos="zoom-in">Phonebook</Title>
           <Form />
-          <Title fw={700} fz={42}>Contacts</Title>
+          <Title fw={700} fz={42} data-aos="zoom-in">Contacts</Title>
           {items.length > 1 && <Text fw={700}>There are {items.length} contacts in the Phonebook.</Text>}
           {items.length === 1 && <Text fw={700}>There is 1 contact in the Phonebook.</Text>}
           {items.length === 0 && <Text fw={700}>There are no contacts in the Phonebook.</Text>}
           <Filter />
           <List contacts={filteredContacts()} />
         </SimpleGrid> 
-      </PhonebookWrapper>
       </>
       
     )
--- src/pages/MainLayout.jsx/MainLayout.styled.jsx
+++
@@ -1,8 +0,0 @@
-import s from '@emotion/styled';
-
-const PhonebookWrapper = s.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-`
-export default PhonebookWrapper;
